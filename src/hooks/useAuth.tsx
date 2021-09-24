7b57e3ecfdf858e7578f69aa29200961d9305247
hedenica <hedenica@hotmail.com>
2021-09-24 19:05:21 -0300

    feat(challenge): authentication and login social completed

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -30,68 +30,117 @@ const twitchEndpoints = {
   revocation: 'https://id.twitch.tv/oauth2/revoke'
 };
 
+const { CLIENT_ID } = process.env
+
 function AuthProvider({ children }: AuthProviderData) {
   const [isLoggingOut, setIsLoggingOut] = useState(false);
   const [isLoggingIn, setIsLoggingIn] = useState(false);
   const [user, setUser] = useState({} as User);
   const [userToken, setUserToken] = useState('');
 
-  // get CLIENT_ID from environment variables
-
   async function signIn() {
     try {
       // set isLoggingIn to true
+      setIsLoggingIn(true)
 
       // REDIRECT_URI - create OAuth redirect URI using makeRedirectUri() with "useProxy" option set to true
+      const REDIRECT_URI = makeRedirectUri({ useProxy: true })
+
       // RESPONSE_TYPE - set to "token"
+      const RESPONSE_TYPE = 'token'
+
       // SCOPE - create a space-separated list of the following scopes: "openid", "user:read:email" and "user:read:follows"
+      const SCOPE = encodeURI('openid user:read:email user:read:follows')
+
       // FORCE_VERIFY - set to true
+      const FORCE_VERIFY = true
+
       // STATE - generate random 30-length string using generateRandom() with "size" set to 30
+      const STATE = generateRandom(30)
 
       // assemble authUrl with twitchEndpoint authorization, client_id, 
       // redirect_uri, response_type, scope, force_verify and state
 
+      const authUrl = twitchEndpoints.authorization + 
+        `?client_id=${CLIENT_ID}` + 
+        `&redirect_uri=${REDIRECT_URI}` + 
+        `&response_type=${RESPONSE_TYPE}` +
+        `&scope=${SCOPE}` + 
+        `&force_verify=${FORCE_VERIFY}` +
+        `&state=${STATE}`;
+
       // call startAsync with authUrl
+
+      const response = await startAsync({ authUrl })
 
       // verify if startAsync response.type equals "success" and response.params.error differs from "access_denied"
       // if true, do the following:
 
+      // console.log('response do startAsync', response)
+
+      if (response.type === 'success' && response.params.error !== "access_denied") {
         // verify if startAsync response.params.state differs from STATE
-        // if true, do the following:
+        if (response.params.state !== STATE) {
+          // if true, do the following:
           // throw an error with message "Invalid state value"
+          throw new Error('Invalid state value')
+        }
 
-        // add access_token to request's authorization header
+        api.defaults.headers.authorization = `Bearer ${response.params.access_token}`
 
-        // call Twitch API's users route
+        const userResponse = await api.get('/users');
 
-        // set user state with response from Twitch API's route "/users"
+        setUser(userResponse.data.data[0])
         // set userToken state with response's access_token from startAsync
-    } catch (error) {
+        setUserToken(response.params.access_token)
+      }
+      
+    } catch (error: any) {
       // throw an error
+      throw new Error(error)
     } finally {
       // set isLoggingIn to false
+      setIsLoggingIn(false)
     }
   }
 
   async function signOut() {
     try {
       // set isLoggingOut to true
+      setIsLoggingOut(true)
+
+      const config = {
+        token: userToken,
+        clientId: CLIENT_ID,
+      }
 
       // call revokeAsync with access_token, client_id and twitchEndpoint revocation
+      revokeAsync(config, {
+        revocationEndpoint: twitchEndpoints.revocation
+      })
     } catch (error) {
+      console.log(error)
     } finally {
       // set user state to an empty User object
+      setUser({} as User)
+      
       // set userToken state to an empty string
+      setUserToken('')
 
       // remove "access_token" from request's authorization header
+      delete api.defaults.headers.authorization
 
       // set isLoggingOut to false
+      setIsLoggingOut(false)
     }
   }
 
   useEffect(() => {
     // add client_id to request's "Client-Id" header
+    api.defaults.headers['Client-Id'] = CLIENT_ID
   }, [])
+
+  // console.log(api.defaults.headers)
 
   return (
     <AuthContext.Provider value={{ user, isLoggingOut, isLoggingIn, signIn, signOut }}>
